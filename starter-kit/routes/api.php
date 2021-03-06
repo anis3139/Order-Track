e8958a97a1b93ecc774ef18f422f9ba351641c15
saroarsagor <39865255+saroarsagor@users.noreply.github.com>
2021-03-06 20:54:19 +0600

    api generate

--- starter-kit/routes/api.php
+++ starter-kit/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\CateogoryController;
+use App\Http\Controllers\Api\BrandController;
+use App\Http\Controllers\Api\CourierCompanyNameController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\VendorController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +21,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']); 
+
+});
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'V1',
+   // 'namespace' => 'Api\V1'
+
+], function ($router) {
+    Route::apiResource('category', CateogoryController::class); 
+    Route::apiResource('brand', BrandController::class); 
+    Route::apiResource('courier-company-name', CourierCompanyNameController::class);
+    Route::apiResource('product', ProductController::class);
+    Route::apiResource('vendor', VendorController::class);
+
 });
